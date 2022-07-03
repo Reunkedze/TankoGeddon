62da1909ad5a526f2e1de0d6b7aa299e356d6e1f
Sergey <reunok2000@gmail.com>
2022-07-04 02:21:42 +0300

    Task 2

---
+++ TankoGeddon/Source/TankoGeddon/TankPawn.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPawn.h"
+#include "Components\StaticMeshComponent.h"
+#include "GameFramework\SpringArmComponent.h"
+#include "Camera\CameraComponent.h"
+
+// Sets default values
+ATankPawn::ATankPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	RootComponent = BodyMesh;
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(BodyMesh);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(BodyMesh);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATankPawn::MoveForward(float Value)
+{
+	TargetAxisValue = Value;
+}
+
+void ATankPawn::MoveRight(float Value)
+{
+	TargetAxisValueRight = Value;
+}
+
+void ATankPawn::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector forwardVector = GetActorForwardVector();
+	FVector rightVector = GetActorRightVector();
+	FVector movePosition = CurrentLocation + forwardVector * MoveSpeed * TargetAxisValue + rightVector * MoveSpeed * TargetAxisValueRight;
+	SetActorLocation(movePosition, true);
+}
